76c24906ed49ec7b656b4e845f01af14cb0edcf1
Fatima Puig <fatima.puig@upr.edu>
2020-11-04 23:08:00 -0500

    Added axios request

--- ui/aa-app/pages/api/login.js
+++ ui/aa-app/pages/api/login.js
@@ -3,10 +3,10 @@ import axios from 'axios'
 const users = [{ id: 1 }, { id: 2 }, { id: 3 }]
 export default async (req, res) => {
     if (req.method === "POST") {
-        // await axios.post('http://backend:5000/login', req.body)
-        //     .then(function (res) { console.log("hit api then") })
-        //     .catch(error => {
-        //     });
+        await axios.post('http://backend:5000/login', req.body)
+            .then(function (res) { console.log("hit api then") })
+            .catch(error => {
+            });
         console.log("Survived api hit.");
         res.status(200).json(users);
     }
